352740d6624f4e72b488348cb287edb70d7acee6
trhung <trh.bkdn>
2019-06-29 20:55:40 +0700

    Da chuyen sang man hinh quet QR code thanh cong, da nhan noi dung QR code thanh cong. TIep tuc nhan chuyen sang man hinh khac

--- samples/src/main/java/com/inqbarna/tablefixheaders/samples/QRcode.java
+++ samples/src/main/java/com/inqbarna/tablefixheaders/samples/QRcode.java
@@ -1,15 +1,139 @@
 package com.inqbarna.tablefixheaders.samples;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class QRcode extends AppCompatActivity {
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class QRcode extends Activity {
+
+    SurfaceView cameraPreview;
+    TextView txtResult;
+    TextView txtCheck;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+
+    Button btnBoiler;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtCheck = (TextView) findViewById(R.id.txtCheck);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+        //Add Event
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    //Request permission
+                    ActivityCompat.requestPermissions(QRcode.this,
+                            new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+
+                if(qrcodes.size() != 0)
+                {
+                    txtResult.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Barcode thisBarCode = qrcodes.valueAt(0);
+                            txtResult.setText(thisBarCode.rawValue);
+
+                            switch (thisBarCode.rawValue){
+                                case "Boiler": {
+                                    //ntent intentBoiler = new Intent(MainActivity.this, BoilerActivity.class);
+                                    //startActivity(intentBoiler);
+                                    txtCheck.setText("Ok");
+                                    break;
+                                }
+                                case "NH3": {
+                                    //Intent intentNH3 = new Intent(MainActivity.this, NH3Activity.class);
+                                    //startActivity(intentNH3);
+                                    txtCheck.setText("Ok");
+                                    break;
+                                }
+                                default:
+                                    txtCheck.setText("Not Ok");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
-
-
 }
