5d0ae08b9675d4f12938fd7e192b444f129ce601
trhung <trh.bkdn>
2019-06-30 21:45:06 +0700

    - Da nhap du lieu thanh cong vao hourly_check table - Xuat du lieu thanh cong cua hourly_check table

--- samples/src/main/java/com/inqbarna/tablefixheaders/samples/HourlyCheckTable.java
+++ samples/src/main/java/com/inqbarna/tablefixheaders/samples/HourlyCheckTable.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import com.inqbarna.tablefixheaders.TableFixHeaders;
 import com.inqbarna.tablefixheaders.samples.adapters.MatrixTableAdapter;
 import com.inqbarna.tablefixheaders.samples.adapters.boilerDataType;
+import com.inqbarna.tablefixheaders.samples.adapters.hourlyCheckType;
 
 import java.util.ArrayList;
 
@@ -17,8 +18,8 @@ public class HourlyCheckTable extends Activity {
 
     Database database;
 
-    ArrayList<boilerDataType> boilerDataTypeArrayList;
-    boilerDataType boilerDataTypeTmp;
+    ArrayList<hourlyCheckType> hourlyCheckTypeArrayList;
+    hourlyCheckType hourlyCheckTypeTmp;
 
 
     @Override
@@ -30,39 +31,42 @@ public class HourlyCheckTable extends Activity {
         database = new Database(this, "data.sqlite", null, 1);
 
         //Tao array list boiler de nhan du lieu tu database
-        boilerDataTypeArrayList = new ArrayList<>();
+        hourlyCheckTypeArrayList = new ArrayList<>();
 
-        Cursor dataFromDatabase = database.GetData("SELECT * FROM boiler");
+        Cursor dataFromDatabase = database.GetData("SELECT * FROM hourly_check");
         while (dataFromDatabase.moveToNext()){
             int id = dataFromDatabase.getInt(0);
-            String name = dataFromDatabase.getString(1);
-            String checkTime = dataFromDatabase.getString(2);
-            Double apSuat = dataFromDatabase.getDouble(3);
+            String dateOfCheck = dataFromDatabase.getString(1);
+            String timeOfCheck = dataFromDatabase.getString(2);
+            String location = dataFromDatabase.getString(3);
+            String personCheck = dataFromDatabase.getString(4);
 
-            boilerDataTypeTmp = new boilerDataType(id, name, checkTime, apSuat);
-            boilerDataTypeArrayList.add(boilerDataTypeTmp);
+            hourlyCheckTypeTmp = new hourlyCheckType(id, dateOfCheck, timeOfCheck, location, personCheck);
+            hourlyCheckTypeArrayList.add(hourlyCheckTypeTmp);
 
-            Log.d(TAG, "onCreate:id " + id + " name: " + name + " checkTime: " + checkTime + " apSuat: " + apSuat);
+            Log.d(TAG, "onCreate:id " + id + " dateOfCheck: " + dateOfCheck + " timeOfCheck: " + timeOfCheck + " location: " + location + " personCheck: " + personCheck);
         }
 
         //Lay data tu database de dua vao array list boiler
         int col = 0;
         int row = 0;
-        int col_MAX = 4;
+        int col_MAX = 5;
 
-        String dataIntable[][] = new String[boilerDataTypeArrayList.size()][col_MAX]; // Cong them mot de chua cho cho header
+        String dataIntable[][] = new String[hourlyCheckTypeArrayList.size()][col_MAX]; // Cong them mot de chua cho cho header
 
-        for (row = 0; row < boilerDataTypeArrayList.size(); row++){
+        for (row = 0; row < hourlyCheckTypeArrayList.size(); row++){
             if (row == 0){
                 dataIntable[row][0] = "ID";
-                dataIntable[row][1] = "Name";
+                dataIntable[row][1] = "Date";
                 dataIntable[row][2] = "Time";
-                dataIntable[row][3] = "Pressure Steam";
+                dataIntable[row][3] = "Location";
+                dataIntable[row][4] = "Person";
             } else {
-                dataIntable[row][0] = String.valueOf(boilerDataTypeArrayList.get(row).getId());
-                dataIntable[row][1] = String.valueOf(boilerDataTypeArrayList.get(row).getName());
-                dataIntable[row][2] = String.valueOf(boilerDataTypeArrayList.get(row).getCheckTime());
-                dataIntable[row][3] = String.valueOf(boilerDataTypeArrayList.get(row).getPressureSteam());
+                dataIntable[row][0] = String.valueOf(hourlyCheckTypeArrayList.get(row).getId());
+                dataIntable[row][1] = String.valueOf(hourlyCheckTypeArrayList.get(row).getDateOfCheck());
+                dataIntable[row][2] = String.valueOf(hourlyCheckTypeArrayList.get(row).getTimeOfCheck());
+                dataIntable[row][3] = String.valueOf(hourlyCheckTypeArrayList.get(row).getLocation());
+                dataIntable[row][4] = String.valueOf(hourlyCheckTypeArrayList.get(row).getPersonCheck());
             }
         }
 
